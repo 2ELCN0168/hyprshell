86939b2904af66a9cdb11a6b5a5d7ee30ab0360a
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-12-04 23:20:34 +0100

    Added files

---
+++ include/env.sh
@@ -0,0 +1,8 @@
+export LESS='-R --use-color -Dd+r$Du+b$'
+export MANPAGER="less -R --use-color -Dd+r -Du+b"
+export MANROFFOPT="-P -c"
+export GREP_COLORS="mt=01;91"
+export EDITOR="vim"
+export VISUAL="vim"
+export GIT_EDITOR="vim"
+export SUDO_PROMPT=$(printf "\n\033[92;1m[?] \033[97mEnter your password \033[92m\n\033[94m[>] \033[0m")
