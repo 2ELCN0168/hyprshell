e022a4812699c2450fc12d817de114949dcdacc9
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-12-05 17:03:12 +0100

    Corrected path

--- include/set_default_theme.sh
+++ include/set_default_theme.sh
@@ -3,5 +3,5 @@
 if [[ "${EUID}" -eq 0 ]]; then
         source "/etc/tty_themes.d/dracula.sh" 
 else
-        source "/etc/tty_themes.d/synth.sh" 
+        source "/etc/tty_themes.d/synthwave86.sh" 
 fi
