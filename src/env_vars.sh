f4e79f279b265b641147479126133012dfbaf05b
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-12-05 11:59:20 +0100

    Debug selinux

--- src/env_vars.sh
+++ src/env_vars.sh
@@ -5,11 +5,11 @@ function set_env_vars()
         cp "../include/env.sh" "/etc/shell_conf.d/"
         cp "../include/aliases.sh" "/etc/shell_conf.d/"
         
-        if [[ "${DETECTED_DISTRO}" -eq 3 ]]; then
-                # RHEL - SELinux context
-                semanage fcontext -a -t etc_t "/etc/shell_conf.d(/.*)?"
-                restorecon -R "/etc/shell_conf.d/"
-        fi
+        # if [[ "${DETECTED_DISTRO}" -eq 3 ]]; then
+        #         # RHEL - SELinux context
+        #         semanage fcontext -a -t etc_t "/etc/shell_conf.d(/.*)?"
+        #         restorecon -R "/etc/shell_conf.d/"
+        # fi
 
         return 0
 }
