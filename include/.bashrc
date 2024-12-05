c69ac90961bd29788392609662c85c4f28c97e99
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-12-05 11:57:44 +0100

    Added conf files

---
+++ include/.bashrc
@@ -0,0 +1,35 @@
+# Do nothing if shell is not interactive
+[[ "${-}" != *i* ]] && return
+
+# History size
+HISTSIZE=10000
+HISTFILESIZE=20000
+
+# Ignore consecutive and identical commands
+HISTCONTROL=ignoredups:erasedups
+
+# Add timestamps to commands in history
+HISTTIMEFORMAT="%d/%m/%Y %H:%M:%S "
+
+# Add history from previous sessions
+shopt -s histappend
+
+# Source every shell/env configuration file in "/etc/shell_conf.d"
+for i in /etc/shell_conf.d/*.sh; do
+        [[ -r "${i}" ]] && source "${i}"
+done
+
+# Prompt
+if [[ "${EUID}" -eq 0 ]]; then
+        # Root account!
+        PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
+        PS1='\[\e[93m\][\@]\[\e[91m\][\[\e[1m\]\u\[\e[0m\] \[\e[97;1;2;3m\]\w\[\e[0;91m\]]\[\e[0m\] \[\e[91;2m\](\[\e[97m\]${PS1_CMD1}\[\e[91m\])\[\e[0m\] \[\e[31;2;3m\]\H\n\[\e[22;91m\]\$\[\e[0m\] '
+elif id -nG "${USER}" | grep -qE '\b(sudo|wheel)\b'; then
+        # Administrative user
+        PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
+        PS1='\[\e[96m\][\@][\[\e[92;1m\]\u\[\e[0m\] \[\e[90;3m\]\w\[\e[0;96m\]]\[\e[0m\] \[\e[92;2m\](${PS1_CMD1})\n\[\e[0;93;3m\]\$\[\e[0m\] '
+else
+        # Normal user
+        PROMPT_COMMAND='PS1_CMD1=$(git branch --show-current 2>/dev/null)'
+        PS1='(\[\e[96m\]\u\[\e[0m\] \[\e[97;2;3m\]\w\[\e[0m\]) \[\e[92;2;3m\]${PS1_CMD1}\n\[\e[22;96m\]\$\[\e[0m\] '
+fi
---
+++ include/aliases.sh
@@ -0,0 +1,185 @@
+# NAVIGATION
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias conf="cd ${HOME}/.config"
+alias pics="cd ${HOME}/Pictures"
+alias vids="cd ${HOME}/Videos"
+alias docs="cd ${HOME}/Documents"
+alias music="cd ${HOME}/Music"
+alias desk="cd ${HOME}/Desktop"
+alias downs="cd ${HOME}/Downloads"
+alias public="cd ${HOME}/Public"
+
+# NETWORK
+alias ipb='printf "\033[92m"; curl -s ifconfig.me; printf "\033[0m"'
+
+# COLORS
+alias diff='command diff --color=auto'
+alias ll='command ls -lh --color=auto' # May be replaced in "REPLACEMENTS"
+alias ls='command ls --color=auto' # May be replaced in "REPLACEMENTS"
+alias ip='command ip -color=auto'
+alias grep='command grep --color=auto'
+alias egrep='command grep -E --color=auto'
+alias fgrep='command fgrep --color=auto'
+
+# THEMES
+alias _themes="__show_themes"
+
+function __show_themes() {
+
+        local themes=("/etc/tty_themes.d/"*)
+        printf "There are ${#themes[@]} themes in /etc/tty_themes.d/:\n"
+        printf "\033[93m"
+        printf "[@] Warning, themes are only available for the TTY.\n"
+        printf "\033[0m"
+        for i in "${themes[@]}"; do
+                local name=$(basename ${i})
+                printf "\033[96m"
+                printf "%s\n" "${name}"
+                printf "\033[0m"
+        done
+        printf "\n"
+}
+
+if [[ "${TERM}" == "linux" ]]; then
+
+        # These aliases are not useful outsdide a TTY
+        alias _tokyo="source /etc/tty_themes.d/tokyonight_storm.sh"
+        alias _latte="source /etc/tty_themes.d/catppuccin_latte.sh"
+        alias _dracula="source /etc/tty_themes.d/dracula.sh"
+        alias _amber="source /etc/tty_themes.d/mono_amber.sh"
+        alias _green="source /etc/tty_themes.d/mono_green.sh"
+        alias _powershell="source /etc/tty_themes.d/powershell.sh"
+        alias _red="source /etc/tty_themes.d/red_impact.sh"
+        alias _ryuuko="source /etc/tty_themes.d/ryuuko.sh"
+        alias _batman="source /etc/tty_themes.d/batman.sh"
+fi
+
+# SAFETY
+alias rm='command rm -i'
+alias cp='command cp -i'
+alias mv='command mv -i'
+
+# CUSTOM OUTPUTS - USED IN ALIASES
+function _ss() {
+        # BRIGHT GREEN
+        printf "\033[92m"
+        command ss "${@}"
+        printf "\033[0m"
+}
+
+function _free() {
+        # BRIGHT YELLOW
+        printf "\033[93m"
+        command free "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+function _df() {
+        # BRIGHT YELLOW
+        printf "\033[93m"
+        command df "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+function _du() {
+        # BRIGHT YELLOW
+        printf "\033[93m"
+        command du "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+# CUSTOM OUTPUTS - STANDARD
+function lsblk() {
+        # BRIGHT CYAN
+        printf "\033[96m"
+        command lsblk "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+function blkid() {
+        # BRIGHT BLUE
+        printf "\033[94m"
+        command blkid "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+function uptime() {
+        # BRIGHT RED
+        printf "\033[91m"
+        command uptime "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+function ps() {
+        # BRIGHT YELLOW
+        printf "\033[93m"
+        command ps "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+
+function ping() {
+        # BRIGHT BLUE
+        printf "\033[94m"
+        command ping "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+function traceroute() {
+        # BRIGHT BLUE
+        printf "\033[94m"
+        command traceroute "${@}"
+        printf "\033[0m"
+        printf "\n"
+}
+
+function reload() {
+        printf "\033[92m"
+        if [[ "${SHELL}" == "/bin/bash" ]]; then
+                source "~/.bashrc"
+                printf "%s" ".bashrc reloaded.\n"
+        elif [[ "${SHELL}" == "/bin/zsh" ]]; then
+                source "~/.zshrc"
+                printf "%s" ".zshrc reloaded.\n"
+        fi
+        printf "\033[0m"
+}
+
+# CONVENIENCE
+alias untar='command tar xvf'
+alias sstcp='_ss -eant'
+alias ssudp='_ss -eanu'
+
+# READABILITY
+alias du='_du -h'
+alias df='_df -h' # May be replaced in "REPLACEMENTS"
+alias free='_free -h'
+
+# REPLACEMENTS
+if command -v eza 1> "/dev/null" 2>&1; then
+        alias ls='eza'
+        alias ll='eza -l'
+fi
+
+if command -v bat 1> "/dev/null" 2>&1; then
+        alias cat='command bat -n --theme=base16-256'
+fi
+
+if command -v btm 1> "/dev/null" 2>&1; then
+        alias btm='command btm -r 250ms -T'
+fi
+
+if command -v duf 1> "/dev/null" 2>&1; then
+        alias df='command duf'
+fi
+# OTHERS
