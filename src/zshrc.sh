d24990ea7330d7b647d6571ec316b5485a7d1754
Enzo <eluciani@cpzardaigoro01o.home.arpa>
2024-12-05 14:21:45 +0100

    Added zshrc support

---
+++ src/zshrc.sh
@@ -0,0 +1,26 @@
+function zshrc()
+{
+        if [[ "${opt_z}" -eq 1 ]] && return
+
+        _message "I" "Copying ${P}.zshrc${N}"
+
+        local dirs=(
+                "/root"
+                "/etc/skel"
+        )
+
+        for i in /home/*; do
+                if [[ -d "${i}" ]]; then
+                        dirs+=("${i}")
+                fi
+        _message "I" "Detected ${P}${i}${N}"
+        done
+        
+        for i in "${dirs[@]}"; do
+                if cp -f "../include/.zshrc" "${i}"; then
+                        _message "S" "Copied ${P}.zshrc${N} to ${P}${i}${N}"
+                else
+                        _message "W" "Could not copy ${P}.zshrc${N} to ${P}${i}${N}"
+                fi
+        done
+}
